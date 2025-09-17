6713b60f725ab0c155160d40b1df9204eff8d5e8
paekung <supakit.3t@gmail.com>
2025-09-17 13:26:46 +0700

    ex00

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,11 @@
+def array_of_names(names):
+    return [f"{first.capitalize()} {last.capitalize()}" for first, last in names.items()]
+
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+
+print(array_of_names(persons))

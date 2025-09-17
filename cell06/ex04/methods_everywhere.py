29bbf978d16cb0276c147d138456413545011912
paekung <supakit.3t@gmail.com>
2025-09-17 13:19:10 +0700

    ex04

---
+++ cell06/ex04/methods_everywhere.py
@@ -0,0 +1,21 @@
+import sys
+
+def shrink(s):
+    print(s[:8])
+
+def enlarge(s):
+    print(s + 'Z' * (8 - len(s)))
+
+if len(sys.argv) == 1:
+    print("none")
+    sys.exit()
+
+for arg in sys.argv[1:]:
+    if len(arg) > 8:
+        shrink(arg)
+    elif len(arg) < 8:
+        enlarge(arg)
+    else:
+        print(arg)
+
+# input arg 'lol' 'physically' 'backpack'

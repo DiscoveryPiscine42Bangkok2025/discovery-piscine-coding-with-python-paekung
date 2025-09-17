07e25efca4fee3f89cea786e63679a7f0cfdb8a3
paekung <supakit.3t@gmail.com>
2025-09-17 16:09:46 +0700

    ex01

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,8 +1,3 @@
-# The program accepts user input. This input is a number, which you will store in a
-# numeric variable.
-# • This number represents the multiplication table you will display. (For example, if
-# the input is 2, you need to display the table for 2)
-
 print("Enter a number")
 num = int(input())
 for i in range(0, 10):
